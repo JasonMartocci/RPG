c54846efb241f3d50f1457d67961e61e83c3a203
Jason Martocci <Jason Martocci>
2016-02-25 13:40:12 -0500

    trying to get attack to work

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,163 +1,108 @@
-var characters = [
-    {
-		name: 'Peashooter',
-		picture: 'http://vignette3.wikia.nocookie.net/plantsvszombies/images/8/85/Peashooter1.png/revision/latest?cb=20111029120055',
-		hitPoints: 100,
-		alive: true, 
-		attackPower: 25,
-		counterPower: 6
-    }, 
-    {
-		name: "Sunflower",
-		picture: 'http://vignette4.wikia.nocookie.net/plantsvszombies/images/e/e2/Sunflower1.png/revision/latest?cb=20090521221033',
-		hitPoints: 110,
-		alive: true, 
-		attackPower: 25,
-		counterPower: 6
-    }, 
-    {
-		name: "Cherry-Bomb",
-		picture: 'http://vignette2.wikia.nocookie.net/plantsvszombies/images/0/0d/Cherry_Bomb1.png/revision/latest?cb=20090521215844',
-		hitPoints: 120,
-		alive: true, 
-		attackPower: 25,
-		counterPower: 6
-    }, 
-    {
-		name: "Chomper",
-		picture: 'http://vignette2.wikia.nocookie.net/plantsvszombies/images/a/a3/Chomper1.png/revision/latest?cb=20090521220057',
-		hitPoints: 130,
-		alive: true, 
-		attackPower: 25,
-		counterPower: 6
-    }, 
-    {
-		name: "Kernel-pult",
-		picture: 'http://vignette4.wikia.nocookie.net/plantsvszombies/images/c/c4/Kernel-pult1.png/revision/latest?cb=20090521220358',
-		hitPoints: 140,
-		alive: true, 
-		attackPower: 25,
-		counterPower: 6
-    }
-  ];
-
-var points = {
-		hitPoints : 0,
-		attackPower : 0,
-		newPower : 0,
-		healthOpponent : 0,
-		counterPower : 0,	
-		selectCounter : 0,
-		opponentNow : false,
-		winCounter : 0,
+var Peashooter = {
+	name: 'Peashooter',
+	health: 180,
+	attackPower: 20,
+	attackPowerConstant: 20,
+	counterAttack: 25
 }
 
-var names = ['Peashooter', 'Sunflower', 'Cherry-Bomb', 'Chomper', 'Kernel-pult'];
-var yourChar = "";    
-var enemyChar = "";  
-
-$(document).ready(function(){
-	// create a new div element and put it into a variable named character
-	var character = document.createElement('div');
-
-	// use the setAttribute function on the character variable and set the id of it to menu
-	character.setAttribute('id', 'menu');
-
-	// add the character to the page with the appendChild function
-	document.body.appendChild(character);
-
-		for (var i = 0; i < characters.length; i++) {
-			// create a div and store it inside a variable named newCharacterDiv
-			var newCharacterDiv = document.createElement('div');
-
-			// use the setAttribute function on the newCharacterDiv variable and set the class of it to character
-			newCharacterDiv.setAttribute('class', 'character');
-			newCharacterDiv.setAttribute('data-name', characters[i].name);
-			newCharacterDiv.setAttribute('hitPoints', characters[i].hitPoints);
-			newCharacterDiv.setAttribute('onclick', 'moveButton(this)');
-			var html = "<p>" + characters[i].name + "</p>"
-			html = html + "<p><img width=\"100\" src=\"" + characters[i].picture + "\" alt=\""+characters[i].name+"\" />"
-			html = html + "<p>Hit Points: " + characters[i].hitPoints + "</p>";
-
-			// set the innerHTML of the newCharacterDiv to html
-			newCharacterDiv.innerHTML = html;
-
-			// add the newCharacterDiv to the character
-			character.appendChild(newCharacterDiv);
-		}
-});
-
-
-// This is the move characters and setup gameplay function
-var good = false;
-
-function moveButton(elem) {
-	if ($(elem).parent().attr("id") == "menu" && !good) {
-		$(elem).detach().appendTo('#good');
-
-		// $('.character').each(function() {
-		//     $(this).attr("id", $(this).attr("id") + "yourChar");
-		// });
-
-	} else if ($(elem).parent().attr("id") == "evil") {
-		$(elem).detach().appendTo('#enemy');
-
-	//enemyChar.push(data-name);
-
-	} else {
-		$(elem).detach().appendTo('#evil');
-	}
-
-	$("#menu").find(".character").detach().appendTo('#evil');
-
-	good = true;
+var CherryBomb = {
+	name: 'CherryBomb',
+	health: 100,
+	attackPower: 10,
+	attackPowerConstant: 10,
+	counterAttack: 5
 }
 
+var Chomper = {
+	name: 'Chomper',
+	health: 150,
+	attackPower: 15,
+	attackPowerConstant: 15,
+	counterAttack: 20
+}
 
-// This is the attack button function
-function attackButton(elem) {
+var KernelPult = {
+	name: 'KernelPult',
+	health: 120,
+	attackPower: 8,
+	attackPowerConstant: 8,
+	counterAttack: 10
+}
 
-	for (var i = 0; i < characters.length; i++) {
-		if ($(elem).parent().parent().attr("id") == "good"){
-			console.log(characters[i].hitPoints);
-		} else if ($(elem).parent().parent().attr("id") == "enemy"){
-			console.log(characters[i].hitPoints);
+function pickHero() {
+	$('.enemies').append($(".choose-hero"));
+	$('.your-char').append(this);
+}
+
+function pickVillain(e){
+	$('.attack-report').empty();
+	$('.counter-report').empty();
+    $('.defender').append(e.target);
+}
+
+function changeHealth() {
+	var hero = $('.your-char div').attr('id');
+	if(hero == 'Peashooter') {hero = Peashooter};
+	if(hero == 'CherryBomb') {hero = CherryBomb};
+	if(hero == 'Chomper') {hero = Chomper};
+	if(hero == 'KernelPult') { hero = KernelPult};
+
+	var villain = $('.defender div').attr('id');
+	if(villain == 'Peashooter') {villain = Peashooter};
+	if(villain == 'CherryBomb') {villain = CherryBomb};
+	if(villain == 'Chomper') {villain = Chomper};
+	if(villain == 'KernelPult') { villain = KernelPult};
+	$('.defender .health').html(villain.health -= hero.attackPower);
+	$('.your-char .health').html(hero.health -= villain.counterAttack);
+	
+	$('.attack-report').html("You attacked " + villain.name + " for " + 
+		hero.attackPower + " damage.");
+	$('.counter-report').html(villain.name + " attacked you back for " +
+		 villain.counterAttack + " damage.");
+
+	hero.attackPower += hero.attackPowerConstant;
+
+	if(hero.health <= 0 || villain.health <= 0) {
+		if(hero.health <= 0) {
+			$('.result').html("YOU LOSE.");
+			$('.btn-attack').remove();
+			$('.fight-section').append('<button class="restart btn btn-danger">Restart</button>');
+			$('.restart').click(restart);
 		} else {
-			alert('your coding is wrong');
+			$('.attack-report').html("You defeated " + villain.name + "!");
+			$('.counter-report').html("Please choose your next opponent.");
+			$('.defender div').remove();
+			$('.enemies').on('click', pickVillain);
+			$('.enemies').click(function(){
+    			$('.enemies').unbind('click', pickVillain);
+    		});
 		}
-			// characters[enemyChar].hitPoints -= characters[yourChar].attackPower;
+		if($('.enemies div').length == 0) {
+			// alert("WINNER!");
+			$('.result').html("WINNER!");
+			$('.btn-attack').remove();
+			$('.fight-section').append('<button class="restart btn btn-danger">Restart</button>');
+			$('.restart').click(restart);
+		}
 	}
 }
 
-// The player will now be able to hit the attack button to fight against that defender.
+function restart() {
+	location.reload();
+}
 
-// Whenever the attack button is hit, the player character is going to attack the defender character once. The defender character will now counter attack the player character once.
+$(document).ready(function() {
 
-// When the player character "attacks", the defender will lose "Health Points" displayed at the bottom of the defender's picture.
+	$('.choose-hero').on('click', pickHero);
+	$('.choose-hero').click(function(){
+        $('.choose-hero').unbind('click', pickHero);
+     });
+	$('.enemies').on('click', pickVillain);
+	$('.enemies').click(function(){
+    	$('.enemies').unbind('click', pickVillain);
+    });
 
-// When the defender "counter attacks", the player character will lose hp displayed at the bottom of the player character's picture.
+	$('.btn-attack').click(changeHealth);
 
-// Keep hitting the attack to try and defeat the defender
-
-// When the defender's "Health Points" are reduced to 0 or below, they are removed from the "defender area". The player character can now pick a new enemy to attack.
-
-// The player wins the game by defeating all enemy characters, and loses the game the game if the player character's "Health Points" are 0 or below.
-
-
-
-
-
-
-// Game design notes:
-
-// Each character in the game has 3 attributes: Health Points, Attack Power, and Counter Attack Power.
-
-// Each time the player attacks, their character's Attack Power increases by its base Attack Power. So if the base Attack Power is 6, each attack will increase the Attack Power by 6. (12, 18, 24, 30 etc...)
-
-// The enemy character only has Counter Attack Power and their Counter Attack Power never changes.
-
-// The Health Points, Attack Power, and Counter Attack Power of each character will be different.
-
-// None of the characters in the game can heal or recover Health Points, so the point of the game is to pick a character and fight an enemy that has low Counter Attack Power first and build up your own Attack Power before you lose all your Health Points.
-
-// Depending on game play, you can win or lose with any of the characters in the game.
+});
